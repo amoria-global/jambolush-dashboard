a55ceea0d6a83d6bc076da55a3a4043cf43cc705
uwitonze8 <pacific.uwitonze112@gmail.com>
2025-08-09 20:35:17 +0200

    ft:add user my bookings

---
+++ app/all/user-mybooking/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import BookingsDashboard from '../../pages/user-mybooking';
+const UserBookingDashboardPage = () => {
+  return (
+    <div>
+      <BookingsDashboard />
+    </div>
+  );
+};
+
+export default UserBookingDashboardPage;
